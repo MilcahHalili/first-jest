96bd8e337d9b4acd2fde8a3e18faea17d295424a
MilcahHalili <halilicenteno@gmail.com>
2019-06-12 09:50:25 -0700

    add hide/show toggle to button and create toggle test

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -8,3 +8,16 @@ describe('my first snapshot test', () => {
     expect(tree.toJSON()).toMatchSnapshot();
   })
 })
+
+describe('changing button name to Hide', () =>{
+  test('toggle the button', () => {
+    const tree = create(<App />);
+    const instance = tree.getInstance();
+    expect(instance.state.isActive).toBe(false)
+    // changing the state
+    instance.handleClick();
+    // isActive property is updated to true
+    expect(instance.state.isActive).toBe(true);
+    expect(tree.toJSON()).toMatchSnapshot()
+  })
+})
